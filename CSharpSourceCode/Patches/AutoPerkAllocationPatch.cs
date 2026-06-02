0130edb7ee82ccf462034f045926c994bc5ba821
alemre <aliemrebahat0033@gmail.com>
2026-06-02 04:24:48 +0200

    1.4 compability patch

--- CSharpSourceCode/Patches/AutoPerkAllocationPatch.cs
+++ CSharpSourceCode/Patches/AutoPerkAllocationPatch.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Reflection;
 using HarmonyLib;
 using TaleWorlds.CampaignSystem;
@@ -6,7 +7,7 @@ using TaleWorlds.CampaignSystem.CampaignBehaviors;
 namespace ProperSkillDistributor
 {
     [HarmonyPatch]
-    public static class AutoPerkAllocationPatch
+    public static class AutoPerkDailyTickPatch
     {
         private static MethodBase TargetMethod()
         {
@@ -15,9 +16,74 @@ namespace ProperSkillDistributor
 
         private static bool Prefix(Hero hero)
         {
+            return AutoPerkGuard.ShouldVanillaAllocatePerks(hero);
+        }
+    }
+
+    [HarmonyPatch]
+    public static class AutoPerkDevelopmentGatePatch
+    {
+        private static MethodBase TargetMethod()
+        {
+            return AccessTools.Method(
+                typeof(CharacterDevelopmentCampaignBehavior),
+                "ShouldDevelopCharacterStats",
+                new[] { typeof(Hero) });
+        }
+
+        private static bool Prepare()
+        {
+            return AccessTools.Method(
+                typeof(CharacterDevelopmentCampaignBehavior),
+                "ShouldDevelopCharacterStats",
+                new[] { typeof(Hero) }) != null;
+        }
+
+        private static bool Prefix(Hero hero, ref bool __result)
+        {
+            if (AutoPerkGuard.ShouldVanillaAllocatePerks(hero))
+            {
+                return true;
+            }
+
+            __result = false;
+            return false;
+        }
+    }
+
+    [HarmonyPatch]
+    public static class AutoPerkCharacterCreationPatch
+    {
+        private static MethodBase TargetMethod()
+        {
+            return AccessTools.Method(
+                typeof(CharacterDevelopmentCampaignBehavior),
+                "OnCharacterCreationIsOver",
+                Type.EmptyTypes);
+        }
+
+        private static bool Prepare()
+        {
+            return AccessTools.Method(
+                typeof(CharacterDevelopmentCampaignBehavior),
+                "OnCharacterCreationIsOver",
+                Type.EmptyTypes) != null;
+        }
+
+        private static bool Prefix()
+        {
+            CampaignOptions.AutoAllocateClanMemberPerks = false;
+            return false;
+        }
+    }
+
+    internal static class AutoPerkGuard
+    {
+        public static bool ShouldVanillaAllocatePerks(Hero hero)
+        {
             if (SkillPresetBehavior.IsPlayerDevelopableHero(hero) && hero != Hero.MainHero)
             {
-                TaleWorlds.CampaignSystem.CampaignOptions.AutoAllocateClanMemberPerks = false;
+                CampaignOptions.AutoAllocateClanMemberPerks = false;
                 return false;
             }
 
